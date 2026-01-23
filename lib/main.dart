a9e6e3b9bac19f567e15067f7125e2b253f7cbe9
ryan <ryangcj07.24@ichat.sp.edu.sg>
2026-01-23 15:05:32 +0800

    Map created

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:mobileappproject/spmap.dart';
+
+void main() async {
+  runApp(MaterialApp(
+    home:const Spmap(),
+  ));
+
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import geolocator_apple
+import location
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
