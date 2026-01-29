eb59376bb58659ffb9b5c0efe0537e301857df98
colin <colino123zn@gmail.com>
2026-01-29 16:01:52 +0800

    startofSPeats

--- lib/foodorder.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Foodorder {
-   static String tappedfood="";
-   static String tappedaddons="";
-
-  static CollectionReference food=FirebaseFirestore.instance.collection('fc3');
-}
