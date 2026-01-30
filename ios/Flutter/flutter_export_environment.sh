5d3da7c63cc0378bca9e5f0020bde4747b923e35
colin <colino123zn@gmail.com>
2026-01-30 14:02:29 +0800

    location cones for user

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Local_Git_Repository\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\rg\Downloads\mobileapp\mobileappproject"
+export "FLUTTER_ROOT=C:\Users\colin\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\colin\Downloads\MAD\speat"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
